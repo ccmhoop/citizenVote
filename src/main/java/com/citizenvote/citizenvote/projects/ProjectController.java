cf8cc3189b3c317977ae625575569060428349a8
janne <ennaj_janne@hotmail.com>
2023-09-11 14:26:05 +0200

    added ProjectController

---
+++ src/main/java/com/citizenvote/citizenvote/projects/ProjectController.java
@@ -0,0 +1,50 @@
+package com.citizenvote.citizenvote.projects;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/project")
+public class ProjectController {
+
+    private final ProjectRepository projectRepository;
+    @Autowired
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloProject() {
+        return ResponseEntity.ok("hello project!");
+    }
+    @GetMapping
+    public ResponseEntity<List<Project>> getAllRecipes() {
+        List<Project> projects = projectRepository.findAll();
+        return ResponseEntity.ok(projects);
+}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Project> getProjectById(@PathVariable Long id) {
+        Project project = projectRepository.findById(id).orElse(null);
+        if (project != null) {
+            return ResponseEntity.ok(project);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PostMapping
+    public ResponseEntity<Project> createRecipe(@RequestBody Project project) {
+        Project savedProject = projectRepository.save(project);
+        return ResponseEntity.ok(savedProject);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        if (projectRepository.existsById(id)) {
+            projectRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
