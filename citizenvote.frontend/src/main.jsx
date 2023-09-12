b4a82b6dd520048e1fa7ebbe196a66b0a2bcb45b
Cannon <markvdvliert@gmail.com>
2023-09-12 14:47:01 +0200

    login pagina werkzaamheden 12-9-23

--- citizenvote.frontend/src/main.jsx
+++ citizenvote.frontend/src/main.jsx
@@ -8,16 +8,21 @@ import './index.css'
 import "./css/main.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './pages/Login';
+import { AuthProvider } from 'react-auth-kit';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+<AuthProvider authType='cookie' authName='_auth' cookieDomain={window.location.hostname} cookieSecure={false}>
   <BrowserRouter>
     <Navbar />
     <div className="flex h-fit justify-center w-screen m-auto">
         <Routes>
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
+          {/* voorbeeld protected Route
+           <Route path="/{beveiligde path}" element={<RequireAuth loginPath="/login"><Pagina/></RequireAuth>}></Route> */}
         </Routes>
     </div>
     <Footer />
   </BrowserRouter>
+</AuthProvider>
 )
