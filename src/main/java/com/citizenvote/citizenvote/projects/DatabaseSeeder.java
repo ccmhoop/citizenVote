11f7dac262aa57f66d7776af1e9694dc0b0cfa9a
janne <ennaj_janne@hotmail.com>
2023-09-12 09:44:50 +0200

    fill in DatabaseSeeder

--- src/main/java/com/citizenvote/citizenvote/projects/DatabaseSeeder.java
+++ src/main/java/com/citizenvote/citizenvote/projects/DatabaseSeeder.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Configuration
@@ -19,7 +20,13 @@ public class DatabaseSeeder {
     }
 
     private void seedDatabase(ProjectRepository projectRepository) {
-        Project project1 = new Project("",);
+        LocalDate startDate1 = LocalDate. of(2023, 9, 12);
+        LocalDate endDate1 = LocalDate. of(2023, 10, 12);
+        LocalDate startDate2 = LocalDate. of(2023, 9, 20);
+        LocalDate endDate2 = LocalDate. of(2023, 10, 20);
+        LocalDate startDate3 = LocalDate. of(2023, 7, 25);
+        LocalDate endDate3 = LocalDate. of(2023, 8, 25);
+        Project project1 = new Project(,"title","description", 50, 0, startDate1, endDate1, Progress.APPROVED,Category.SPORTS);
         Project project2 = new Project();
         Project project3 = new Project();
 
