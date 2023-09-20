1f193614303d5e178a2e712602aeb0a32b750d91
janne <ennaj_janne@hotmail.com>
2023-09-20 15:24:09 +0200

    token buck

---
+++ citizenvote.frontend/src/js/getToken.js
@@ -0,0 +1,16 @@
+export const getToken = () => {
+  if (document.cookie) {
+    var cookieArr = document.cookie.split(";");
+    for (const cookie of cookieArr) {
+      const cookienv = cookie.split("=");
+      if (cookienv[0] === "_auth") {
+        console.log(cookienv[1].length);
+        return {
+          token: cookienv[1],
+          cfg: { headers: { Authorization: `Bearer ${cookienv[1]}` } },
+        };
+      }
+    }
+  }
+  return { token: "", cfg: { headers: { Authorization: "" } } };
+};
